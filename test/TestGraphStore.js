ae2b7e9b74c51e311e9de017edb73240a28c4e7b
Robert Fromont <robert@fromont.net.nz>
2020-02-07 15:16:29 -0300

    Imporve unit tests.

--- test/TestGraphStore.js
+++ test/TestGraphStore.js
@@ -1,19 +1,34 @@
 'use strict';
 
-var expect = require('chai').expect;
-var labbcat = require('../nzilbb.labbcat');
-var baseUrl = "http://localhost:8080/labbcat/";
-var username = "labbcat";
-var password = "labbcat";
+const assert = require('chai').assert;
+const labbcat = require('../nzilbb.labbcat');
+const baseUrl = "http://localhost:8080/labbcat/";
+const username = "labbcat";
+const password = "labbcat";
+var store = null;
 
 describe("#GraphStore", ()=>{
+    before((done)=>{
+        store = new labbcat.GraphStore(baseUrl, username, password);
+        store.getId((id, errors, messages)=>{
+            assert.isNull(errors, "\nCould not connect to LaBB-CAT."
+                          +"\nThese unit tests require a running LaBB-CAT server to test against."
+                          +"\nPlease check the baseUrl variable refers to a running LaBB-CAT server.");
+            done();
+        });
+    });
+
     beforeEach((done)=>{
         // verbosity only applies in tests that enable it
         labbcat.verbose = false;
         done();
     });
 
-    it("exports GraphStore", ()=>{
-        expect(labbcat.GraphStore).to.exist;
+    it("inherits methods (getId at least)", (done)=>{
+        store.getId((result, errors, messages, call)=>{
+            assert.isNull(errors);
+            assert.equal(baseUrl, result);
+            done();
+        });
     });
 });
--- test/TestLabbcat.js
+++ test/TestLabbcat.js
@@ -1,26 +1,33 @@
 'use strict';
 
-var expect = require('chai').expect;
-var labbcat = require('../nzilbb.labbcat');
-var baseUrl = "http://localhost:8080/labbcat/";
-var username = "labbcat";
-var password = "labbcat";
+const assert = require('chai').assert;
+const labbcat = require('../nzilbb.labbcat');
+const baseUrl = "http://localhost:8080/labbcat/";
+const username = "labbcat";
+const password = "labbcat";
+var lc = null;
 
 describe("#Labbcat", ()=>{
+    before((done)=>{
+        lc = new labbcat.Labbcat(baseUrl, username, password);
+        lc.getId((id, errors, messages)=>{
+            assert.isNull(errors, "\nCould not connect to LaBB-CAT."
+                          +"\nThese unit tests require a running LaBB-CAT server to test against."
+                          +"\nPlease check the baseUrl variable refers to a running LaBB-CAT server.");
+            done();
+        });
+    });
+    
     beforeEach(function(done) {
         // verbosity only applies in tests that enable it
         labbcat.verbose = false;
         done();
     });
     
-    it("exports Labbcat", ()=>{
-        expect(labbcat.Labbcat).to.exist;
-    });
-
-    it("support getId", (done)=>{
-        const lc = new labbcat.Labbcat(baseUrl, username, password);
+    it("inherits methods (getId at least)", (done)=>{
         lc.getId((result, errors, messages, call)=>{
-            expect(result).to.equal(baseUrl);
+            assert.isNull(errors);
+            assert.equal(result, baseUrl);
             done();
         });
     });
